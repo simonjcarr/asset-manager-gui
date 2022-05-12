be7661ee7f6ff518d5602705b785dee0316448c2
simonjcarr <simonjcarr@gmail.com>
2022-05-12 19:59:38 +0100

    Added netlify identity

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div class="flex flex-col min-h-screen bg-primaryBackground text-primaryText">
     <div class="text-2xl">This is the header</div>
     <div class="p-5">
+      <div data-netlify-identity-menu></div>
       <router-view></router-view>
     </div>
   </div>
